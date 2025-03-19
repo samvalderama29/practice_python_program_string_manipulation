bf9f4ff406b87698a8781eab63fa22cef51a172c
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 10:42:33 +0800

    docs: add comments to explain lowercase_fullname.py

--- lowercase_fullname.py
+++ lowercase_fullname.py
@@ -1,8 +1,11 @@
 # Prog04: Create a program that ask the user to input their fullname.
 #         Print the input in all lower case.
 
+# Display message to indicate the purpose of the program
 print("Fullname in all lower case letters")
 
+# Prompt the user to enter a number from 0 - 1000
 fullname = input("Enter your name: ")
 
+# Convert the input name to uppercase using the upper() method and print the result
 print(fullname.lower())
