e833c96671ac8fb29d3176b06c3c7b5331b5d075
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 16:19:52 +0800

    docs: add comments to explain snake_case_fullname.py

--- snake_case_fullname.py
+++ snake_case_fullname.py
@@ -1,10 +1,14 @@
 # Prog10: Create a program that ask the user to input their fullname in incorrect casing.
 #         Print the input in snake case.
 
+# Display message to indicate the purpose of the program
 print("Full name in pascal case")
 
+# Prompt the user to enter their full name
 fullname = input("Enter your full name: ")
 
+# Convert the full name to lowercase
 lowercase_name = fullname.lower()
 
+# Replace spaces with underscores to convert to snake_case and print the result
 print(lowercase_name.replace(" ", "_"))
