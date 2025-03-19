86281c7cf7739018a76b4c6ead4db06299ce8f18
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 10:38:53 +0800

    docs: add comments to explain uppercase_fullname.py

--- uppercase_fullname.py
+++ uppercase_fullname.py
@@ -1,8 +1,11 @@
 # Prog03: Create a program that ask the user to input their fullname.
 #         Print the input in all capital letter.
 
+# Display message to indicate the purpose of the program
 print("Fullname in all capital letters")
 
+# Prompt the user to enter a number from 0 - 1000
 fullname = input("Enter your name: ")
 
+# # Convert the input name to uppercase using the upper() method and print the result
 print(fullname.upper())
