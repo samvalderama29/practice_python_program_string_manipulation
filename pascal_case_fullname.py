1c93d46cbb0fe0b754f3f6224c2e16eba71a8190
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 12:24:23 +0800

    init: add pascal_case_fullname.py with program description and variable name

--- pascal_case_fullname.py
+++ pascal_case_fullname.py
@@ -1,2 +1,6 @@
 # Prog09: Create a program that ask the user to input their fullname in incorrect casing.
 #         Print the input in pascal case.
+
+print("Full name in pascal case")
+
+fullname = input("Enter your full name: ")
