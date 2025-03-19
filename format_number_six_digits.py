9f52f24323e64b23ca9e5f96f4aad6dbe972831c
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 10:33:09 +0800

    docs: add comments to explain format_number_six_digits.py

--- format_number_six_digits.py
+++ format_number_six_digits.py
@@ -2,8 +2,11 @@
 #         Print the number in 6 digit format.
 #         Add zeros at the beginning to complete the 6 digit.
 
+# Display message to indicate the purpose of the program
 print("Number with 6 digit beginning at 0")
 
+# Prompt the user to enter a number from 0 - 1000
 user_num_input = input("Enter a number (0 - 1000): ")
 
+# Use zfill(6) to ensure the number is displayed with a total of 6 digits, filling with leading zeros if necessary
 print(user_num_input.zfill(6))
