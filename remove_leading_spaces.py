cb7da9a79e14920201efccaae5a9ec83d286e250
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 10:22:05 +0800

    docs: add comments to explain remove_leading_spaces.py

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -1,8 +1,11 @@
 # Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning.
 #         Print the input without the spaces in the beginning.
 
+# Display message to indicate the purpose of the program
 print("Fullname without spaces")
 
+# Prompt the user to enter their full name with leading spaces (Ex:     Sam)
 fullname = input("Enter your full name (must have several space characters at the beginning): ")
 
+# Remove leading spaces using the lstrip() method and print the result
 print(fullname.lstrip())
