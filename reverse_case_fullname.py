0b0a14ec130e47a0355cded0bbaeab8c2a2f2e7d
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 10:52:47 +0800

    init: add reverse_case_fullname.py with program description and variable name

--- reverse_case_fullname.py
+++ reverse_case_fullname.py
@@ -1,2 +1,6 @@
 # Prog06: Create a program that ask the user to input their fullname in incorrect casing.
 #         Print each character of the input in reverse casing.
+
+print("Fullname in reverse casing")
+
+fullname = input("Enter your full name in incorrect casing (Ex: aNgeL): ")
