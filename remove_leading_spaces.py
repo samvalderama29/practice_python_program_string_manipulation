01ce53e4df024985848fb4750190cbc2815c189c
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 10:10:32 +0800

    init: add remove_leading_spaces.py with program description and variable name

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -1,2 +1,6 @@
 # Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning.
 #         Print the input without the spaces in the beginning.
+
+print("Fullname without spaces")
+
+fullname = input("Enter your full name (must have several space characters at the beginning: ")
