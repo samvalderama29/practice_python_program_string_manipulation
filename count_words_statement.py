f975ab4051bd82386a479abe6509f9a4e98e3fd2
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 12:10:37 +0800

    docs: add comments to explain count_words_statement.py

--- count_words_statement.py
+++ count_words_statement.py
@@ -1,10 +1,14 @@
 # Prog07: Create a program that ask the user to input a complete statement.
 #         Print the number of words in the input.
 
+# Display message to indicate the purpose of the program
 print("Count the number of words in a statement")
 
+# Prompt the user to enter a statement
 count_statement = input("Input a statement: ")
 
+# Use split() to break the statement into a list of words
 count_total = count_statement.split()
 
+# Use len() to count the number of words in the list and print the result
 print(len(count_total))
