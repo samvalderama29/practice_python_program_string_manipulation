febc193f718dcb5034b93eb3a4bd29708ec1dde2
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 10:44:53 +0800

    init: add proper_case_fullname.py with program description and variable name

--- proper_case_fullname.py
+++ proper_case_fullname.py
@@ -1,2 +1,6 @@
 # Prog05: Create a program that ask the user to input their fullname in incorrect casing.
 #         Print the input in proper casing.
+
+print("Fullname proper casing")
+
+fullname = input("Enter your full name in incorrect casing (Ex: aNgeL: ")
