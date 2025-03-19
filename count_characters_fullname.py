0b54f6f4d6998925ad42e554320e6dc5760779c7
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 12:15:32 +0800

    init: add count_characters_fullname.py with program description and variable name

--- count_characters_fullname.py
+++ count_characters_fullname.py
@@ -1,2 +1,6 @@
 # Prog08: Create a program that ask the user to input their fullname.
 #         Print the number of characters in the input.
+
+print("No. of characters in a full name")
+
+fullname = input("Enter your full name: ")
