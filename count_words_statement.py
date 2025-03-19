f12af52de46f799f555b459b978d248e2afbac45
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 10:57:00 +0800

    init: add count_words_statement.py with program description and variable name

--- count_words_statement.py
+++ count_words_statement.py
@@ -1,2 +1,6 @@
 # Prog07: Create a program that ask the user to input a complete statement.
 #         Print the number of words in the input.
+
+print("Count the number of words in a statement")
+
+count_statement = input("Input a statement: ")
