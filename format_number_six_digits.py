c86fb1a0d6a6eacd8e800ca978705c032f666c6f
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 10:25:40 +0800

    init: add format_number_six_digits.py with program description and variable name

--- format_number_six_digits.py
+++ format_number_six_digits.py
@@ -1,3 +1,7 @@
 # Prog02: Create a program that ask the user to input a number (0-1000).
 #         Print the number in 6 digit format.
 #         Add zeros at the beginning to complete the 6 digit.
+
+print("Number with 6 digit beginning at 0")
+
+user_num_input = int(input("Enter a number (0 - 1000): "))
