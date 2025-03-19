2400d9b00b8a21b7ef6fea7b32416d2dcd115dd3
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 12:20:21 +0800

    docs: add comments to explain count_characters_fullname.py

--- count_characters_fullname.py
+++ count_characters_fullname.py
@@ -1,8 +1,11 @@
 # Prog08: Create a program that ask the user to input their fullname.
 #         Print the number of characters in the input.
 
+# Display message to indicate the purpose of the program
 print("No. of characters in a full name")
 
+# Prompt the user to enter their full name
 fullname = input("Enter your full name: ")
 
+# Use len() to count the total number of characters (including spaces) in the full name and print the result
 print(len(fullname))
