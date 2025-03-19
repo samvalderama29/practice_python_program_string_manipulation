3d399d39b6af221527230735899f94c7f345989e
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 16:07:28 +0800

    init: add snake_case_fullname.py with program description and variable name

--- snake_case_fullname.py
+++ snake_case_fullname.py
@@ -1,2 +1,6 @@
 # Prog10: Create a program that ask the user to input their fullname in incorrect casing.
 #         Print the input in snake case.
+
+print("Full name in pascal case")
+
+fullname = input("Enter your full name: ")
