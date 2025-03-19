97e007a14d9e5d2f957fc83534d3529cf8200e7c
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 10:50:46 +0800

    docs: add comments to explain proper_case_fullname.py

--- proper_case_fullname.py
+++ proper_case_fullname.py
@@ -1,8 +1,12 @@
 # Prog05: Create a program that ask the user to input their fullname in incorrect casing.
 #         Print the input in proper casing.
 
+# Display message to indicate the purpose of the program
 print("Fullname proper casing")
 
+# Prompt the user to enter their full name with incorrect casing
+# Example: User enters "aNgeL" instead of "Angel"
 fullname = input("Enter your full name in incorrect casing (Ex: aNgeL): ")
 
+# Convert the first letter to uppercase and the rest to lowercase using capitalize() method
 print(fullname.capitalize())
