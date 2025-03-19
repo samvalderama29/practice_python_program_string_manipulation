f17ae882eec622b62e0190c8464d0dc36d935c12
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-19 10:35:04 +0800

    init: add uppercase_fullname.py with program description and variable name

--- uppercase_fullname.py
+++ uppercase_fullname.py
@@ -1,2 +1,6 @@
 # Prog03: Create a program that ask the user to input their fullname.
 #         Print the input in all capital letter.
+
+print("Fullname in all capital letters")
+
+fullname = input("Enter your name: ")
